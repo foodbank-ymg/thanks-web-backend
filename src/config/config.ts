3c4b1fe566f7e485c9ffd22887cdd22f4801eccc
tetsuyanh <tetsuyanh@gmail.com>
2023-01-25 23:24:00 +0900

    fix structure

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,23 +1,25 @@
-import { ClientConfig } from "@line/bot-sdk";
-
 type Config = {
-  closedLine: ClientConfig;
-  publicLine: ClientConfig;
+  adminLineSecret: string;
+  adminLineAccessToken: string;
+  userLineSecret: string;
+  userLineAccessToken: string;
 };
 
+var config: Config | undefined;
+
 export const loadConfig = (): Config => {
-  if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
+  if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
   }
 
-  return {
-    closedLine: {
-      channelSecret: process.env.CLOSED_LINE_SECRET,
-      channelAccessToken: process.env.CLOSED_LINE_TOKEN,
-    },
-    publicLine: {
-      channelSecret: process.env.PUBLIC_LINE_SECRET,
-      channelAccessToken: process.env.PUBLIC_LINE_TOKEN,
-    },
-  };
+  if (!config) {
+    config = {
+      adminLineSecret: process.env.CLOSED_LINE_SECRET,
+      adminLineAccessToken: process.env.CLOSED_LINE_TOKEN,
+      userLineSecret: process.env.PUBLIC_LINE_SECRET,
+      userLineAccessToken: process.env.PUBLIC_LINE_TOKEN,
+    };
+  }
+
+  return config;
 };
--- src/handler/handler.ts
+++ src/handler/handler.ts
@@ -1,25 +1,33 @@
-import { Client, middleware, MiddlewareConfig } from "@line/bot-sdk";
-import express, { Express } from "express";
-import { loadConfig } from "../config/config";
-import { adminLineHandler } from "./admin_line/admin_line";
-import { batchHandler } from "./batch/batch";
-import { userLineHandler } from "./user_line/user_line";
+import { Client, middleware } from '@line/bot-sdk';
+import express from 'express';
+import { loadConfig } from '../config/config';
+import { adminLineHandler } from './admin_line/admin_line';
+import { userLineHandler } from './user_line/user_line';
 export const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
 const config = loadConfig();
-const adminClient = new Client(config.closedLine);
-const adminMiddleware = middleware(config.closedLine as MiddlewareConfig);
-const userClient = new Client(config.publicLine);
-const userMiddleware = middleware(config.publicLine as MiddlewareConfig);
 
-adminLineHandler(app, adminMiddleware, adminClient);
-userLineHandler(app);
-batchHandler(app);
+const adminMiddleware = middleware({ channelSecret: config.adminLineSecret });
+const adminClient = new Client({
+  channelAccessToken: config.adminLineAccessToken,
+  channelSecret: config.adminLineSecret,
+});
+app.post(
+  '/admin-line',
+  adminMiddleware,
+  new adminLineHandler(adminClient).handle,
+);
 
-app.get("/batch/ping", (req, res) => {
-  res.send("pong");
+const userMiddleware = middleware({ channelSecret: config.userLineSecret });
+const userClient = new Client({
+  channelAccessToken: config.userLineAccessToken,
+  channelSecret: config.userLineSecret,
+});
+app.post('/user-line', userMiddleware, new userLineHandler(userClient).handle);
+
+// TODO: 仕様が固まり次第着手します
+// app.post('/batch', middleware, (req, res) => lineEvent(client, req, res));
+
+app.get('/batch/ping', (req, res) => {
+  res.send('pong');
 });
