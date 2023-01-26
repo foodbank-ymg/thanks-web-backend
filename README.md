2f51b52d98695338f883867053c2f0f12769dfe9
Nekodigi <38092328+Nekodigi@users.noreply.github.com>
2023-01-26 20:39:23 +0900

    ts-node nodemon fix

---
+++ README.md
@@ -0,0 +1,10 @@
+# thanks-web-backend
+
+## 環境変数
+
+ローカルでの開発時は、.env ファイルから環境変数を読み込みます。  
+.env.boilerplate を参照ください。
+
+## リンク
+
+- [Schema](https://github.com/foodbank-ymg/thanks-web-backend/wiki/Schema)
--- src/handler/handler.ts
+++ src/handler/handler.ts
@@ -1,8 +1,8 @@
-import { Client, middleware } from '@line/bot-sdk';
-import express from 'express';
-import { loadConfig } from '../config/config';
-import { managerLineHandler } from './manager_line/manager_line';
-import { recipientLineHandler } from './recipient_line/recipient_line';
+import { Client, middleware } from "@line/bot-sdk";
+import express from "express";
+import { loadConfig } from "../config/config";
+import { managerLineHandler } from "./manager_line/manager_line";
+import { recipientLineHandler } from "./recipient_line/recipient_line";
 export const app = express();
 
 const config = loadConfig();
@@ -15,9 +15,9 @@ const managerClient = new Client({
   channelSecret: config.managerLineSecret,
 });
 app.post(
-  '/manager-line',
+  "/manager-line",
   managerMiddleware,
-  new managerLineHandler(managerClient).handle,
+  new managerLineHandler(managerClient).handle
 );
 
 const recipientMiddleware = middleware({
@@ -28,14 +28,14 @@ const recipientClient = new Client({
   channelSecret: config.recipientLineSecret,
 });
 app.post(
-  '/recipient-line',
+  "/recipient-line",
   recipientMiddleware,
-  new recipientLineHandler(recipientClient).handle,
+  new recipientLineHandler(recipientClient).handle
 );
 
 // TODO: 仕様が固まり次第着手します
 // app.post('/batch', middleware, (req, res) => lineEvent(client, req, res));
 
-app.get('/batch/ping', (req, res) => {
-  res.send('pong2');
+app.get("/batch/ping", (req, res) => {
+  res.send("pong2");
 });
