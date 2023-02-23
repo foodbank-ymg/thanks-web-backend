cec97b9d98679ec337005f320f8f1420644be2d3
Nekodigi <38092328+Nekodigi@users.noreply.github.com>
2023-02-23 19:56:23 +0900

    comprehensive test case

--- src/handler/manager_line/setup.test.ts
+++ src/handler/manager_line/setup.test.ts
@@ -1,6 +1,22 @@
-import { confirmName, decideName, tellWelcomeBack } from './setup'
+import {
+  askName,
+  askNameAgain,
+  confirmName,
+  decideName,
+  tellWelcome,
+  tellWelcomeBack,
+} from './setup'
 
 test('line setup message', async () => {
+  expect(tellWelcome()).toMatchObject({
+    type: 'text',
+    text: '友だち追加ありがとうございます。\nこのアカウントでは、文章や画像をチャット送っていただくだけで記事投稿が出来ます。',
+  })
+  expect(askName()).toMatchObject({
+    type: 'text',
+    text: '早速ですが、お名前を教えてください。※この情報はWebサイトには表示されません。',
+  })
+
   expect(tellWelcomeBack('name')).toMatchObject({
     type: 'text',
     text: 'nameさん、おかえりなさい。',
@@ -17,5 +33,10 @@ test('line setup message', async () => {
       text: 'お名前はhogeでよろしいですか？',
     },
   })
+  expect(askNameAgain()).toMatchObject({
+    type: 'text',
+    text: 'もう一度お名前を教えてください。',
+  })
+
   expect(decideName('foo')).toMatchObject({ type: 'text', text: 'fooさん、よろしくお願いします。' })
 })
