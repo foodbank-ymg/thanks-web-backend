ab008b1d7c502a6bb0403d99dafdfc6dd742086e
Nekodigi <uekaz@outlook.jp>
2023-06-10 17:06:37 +0900

    format, jst fix

--- src/lib/sheet/log.ts
+++ src/lib/sheet/log.ts
@@ -1,3 +1,4 @@
+import moment from 'moment'
 import { Post } from '../../types/post'
 import { Recipient } from '../../types/recipient'
 import { getRecipientGroupById } from '../firestore/recipientGroup'
@@ -13,14 +14,14 @@ export const insertLog = async (user: string, action: string, target: string) =>
   //   requestBody: resource,
   // })
   //to add first row
-  insertRow(1, [new Date().toLocaleString(), user, action, target])
+  insertRow(1, [moment().utcOffset(9).format('YYYY/M/DD h:mm:ss'), user, action, target])
 }
 
 export const recipientSummary = async (recipient: Recipient) => {
   const groupName = (await getRecipientGroupById(recipient.recipientGroupId)).name
-  return `受給団体${groupName}, 受給者${recipient.name}`
+  return `${groupName}_${recipient.name}`
 }
 
 export const postSummary = (post: Post) => {
-  return `投稿ID:${post.id}, 主題:${post.subject}`
+  return `${post.id}_${post.subject}`
 }
